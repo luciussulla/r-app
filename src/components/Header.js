a3815908eb8da22068ec8b5da232906562fc8cc8
luciussulla <maciek.kowalczyk@yandex.com>
2021-06-14 20:26:14 +0200

    Make routing for Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import {Route, Switch} from 'react-router-dom'
+import image1 from '../images/header1.jpg'
+import image2 from '../images/header2.jpg'
+import image3 from '../images/header3.jpg'
+import '../styles/Header.css'
 
 const Header = ()=> {
+  const images = [image1, image2, image3]
+  const index = Math.floor(Math.random()*images.length)
+  const img = images[index]
+
   return (
-    <div>blabla</div>
+    <>
+    <Switch>
+      <Route path="/" exact render={()=> (
+        <img src={img} alt="image"/>
+      )} />
+
+      <Route path="/contact" render={()=> (
+        <img src={img} alt="image"/>
+      )} />
+
+      <Route path="/products" render={()=> (
+        <img src={img} alt="image"/>
+      )} />
+
+      <Route path="/admin" render={()=> (
+        <img src={img} alt="image"/>
+      )} />
+
+      <Route render={()=> (
+        <img src={img} alt="image"/>
+      )} />
+    </Switch>
+    </>
   )
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import '../styles/Navigation.css'
+
+const list = [
+  {name: "star", 
+   path: "/", 
+   exact: true
+  },
+  {name: "products", 
+   path: "/products"
+  },
+  {name: "contact", 
+   path: "/contact"
+  },
+  {name: "admin", 
+   path: "/admin"
+  },
+]
 
 export default function Navigation() {
+  const menu = list.map(item=> (
+      <li key={item.name}>
+        <NavLink to={item.path} exact={item.exact ? item.exact : false}>{item.name}</NavLink>
+      </li>
+  ))
+    
   return (
     <div>
-      NAVIGATION
+      <nav className="main">
+        <ul>
+          {menu}
+        </ul>
+      </nav>
     </div>
   )
 }
