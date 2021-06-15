f98beb0064222d8339e308f439f46087056abbb0
luciussulla <maciek.kowalczyk@yandex.com>
2021-06-15 14:45:29 +0200

    Add prompt component to form submit on the contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,51 @@
 import React, { Component } from 'react'
+import "../styles/ContactPage.css"
+import {Prompt} from 'react-router-dom'
 
-export default function ContactPage() {
-  return (
-    <div>
-      Contatc
-    </div>
-  )
+class Contact extends Component {
+  state = {
+    value: "", 
+    isEmpty: true
+  }
+
+  handleChange = (e)=> {
+    if(e.target.value.length>0) {
+      this.setState({
+        value: e.target.value,
+        isEmpty: false,
+      })
+      console.log("is empty should be false")
+    } else {
+      this.setState({
+        value: "",
+        isEmpty: true,
+      })
+    }
+  }
+
+  handleSubmit = (e)=> {
+    e.preventDefault()
+    this.setState({
+      value: "", 
+      isEmpty: true
+    })
+  }
+
+  render() {
+    return (
+      <div className="contact">
+        <form onSubmit={this.handleSubmit}>
+          <h3>Napisz do nas</h3>
+          <textarea value={this.state.value} onChange={this.handleChange} placeholder="Wpisz wiadomość"></textarea>
+          <button>Wyślij</button>
+        </form>
+        <Prompt 
+          when={!this.state.isEmpty}
+          message= "Masz niewypelniony form czy na pewno chcesz opuscic ta strone"
+        />
+      </div>
+    )
+  }
 }
+
+export default Contact
