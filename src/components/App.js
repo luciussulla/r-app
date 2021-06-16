6819b0b3bef9302ac422ef91f727376486563c67
luciussulla <maciek.kowalczyk@yandex.com>
2021-06-16 10:58:24 +0200

    Add basename to Router component

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import Footer from './Footer'
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
     <div className="app">
       <header>
         {<Header/>}
